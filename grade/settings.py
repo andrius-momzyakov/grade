b873934d069134a9a6bf0f828ddc2214febabe0a
Andrew Momzyakoff <andrius.momzyakov@gmail.com>
2017-08-12 13:12:06 +0300

    Страница логина.

--- grade/settings.py
+++ grade/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -57,7 +56,7 @@ ROOT_URLCONF = 'grade.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['common_templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'common_templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
