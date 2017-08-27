290f6baa56aa0c6e274de99efaada7055c007e41
Andrew Momzyakoff <andrius.momzyakov@gmail.com>
2017-08-27 21:55:02 +0300

    тест для sendmail

---
+++ email.py
@@ -0,0 +1,22 @@
+# Import smtplib for the actual sending function
+import smtplib
+
+# Import the email modules we'll need
+from email.mime.text import MIMEText
+
+# Open a plain text file for reading.  For this example, assume that
+# the text file contains only ASCII characters.
+with open(textfile) as fp:
+    # Create a text/plain message
+    msg = MIMEText('TEST')
+
+me == 'andrius-smth@yandex.ru'
+you == 'andrius.momzyakov@gmail.com'
+msg['Subject'] = 'test'
+msg['From'] = me
+msg['To'] = you
+
+# Send the message via our own SMTP server.
+s = smtplib.SMTP('localhost')
+s.send_message(msg)
+s.quit()
